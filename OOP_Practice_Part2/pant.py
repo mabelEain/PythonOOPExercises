21e5bdc15a990a00832568144c9ee75a1e92edbf
Mabel Eain <31437783+mabelEain@users.noreply.github.com>
2021-08-06 23:56:24 +0630

    add pant.py with todo steps

---
+++ OOP_Practice_Part2/pant.py
@@ -0,0 +1,24 @@
+### TODO:
+#   - code a Pants class with the following attributes
+#   - color (string) eg 'red', 'yellow', 'orange'
+#   - waist_size (integer) eg 8, 9, 10, 32, 33, 34
+#   - length (integer) eg 27, 28, 29, 30, 31
+#   - price (float) eg 9.28
+
+### TODO: Declare the Pants Class 
+
+### TODO: write an __init__ function to initialize the attributes
+
+### TODO: write a change_price method:
+#    Args:
+#        new_price (float): the new price of the shirt
+#    Returns:
+#        None
+
+### TODO: write a discount method:
+#    Args:
+#        discount (float): a decimal value for the discount. 
+#            For example 0.05 for a 5% discount.
+#
+#    Returns:
+#        float: the discounted price
